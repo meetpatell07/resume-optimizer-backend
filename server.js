16c7f5d973274cd02b88e39c1a1ec888de1c8fc4
Pmack0702 <meetpatel7026@gmail.com>
2025-03-27 15:30:16 -0700

    server.js Updated

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 
+const technicalKnowledgeRoutes = require('./routes/technicalKnowledgeRoutes');
+const educationRoutes = require('./routes/educationRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const volunteerWorkRoutes = require('./routes/volunteerWorkRoutes')
+const workExperienceRoute = require('./routes/workExperienceRoutes')
+const skillRoute = require('./routes/skillRoutes')
+
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -34,6 +42,34 @@ app.use('/api/jobs', jobRoutes);
 // User Route
 app.use('/api', userRoutes);
 
+// Technical Route
+app.use('/api/v1/technical', technicalKnowledgeRoutes);
+
+// Eductional Route
+app.use('/api/v1/education', educationRoutes);
+
+// Project Route
+app.use('/api/v1/projects', projectRoutes);
+
+// Volunteering Route
+app.use('/api/v1/volunteer', volunteerWorkRoutes);
+
+// Work Experience Route
+app.use('/api/v1/work', workExperienceRoute);
+
+// Skill Route
+app.use('/api/v1/skill', skillRoute);
+
+// You can define any other routes here as needed
+app.get('/', (req, res) => {
+  res.send('Welcome to the Resume Optimizer');
+});
+
+// Error handling middleware (Optional)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
 // Start server
 app.listen(PORT, () => {
